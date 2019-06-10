8937f237d27038e8c17b9bb28fd745b177d93c95
Vagner Santos <santos.vagnerferreira@gmail.com>
2019-06-09 22:50:31 -0300

    feat(player): Draws first player on canvas

--- app/src/game.js
+++ app/src/game.js
@@ -1,5 +1,20 @@
-import {size} from 'play2d';
+import {
+  start,
+  size,
+  canvasWith,
+  image,
+  at,
+  eachFrame,
+  paint,
+  gameObject
+} from 'play2d'
+import playerImg from '../img/player.png'
 
-window.addEventListener("DOMContentLoaded", () => {
-  console.log(size(300, 200));
-});
+window.addEventListener('DOMContentLoaded', () => {
+  canvasWith(800, 600)
+
+  const player = gameObject(image(playerImg), at(0, 0), size(15, 80))
+  eachFrame(() => paint(player()))
+
+  start()
+})
